71876a8ac86cc34bf605453fe25f525f17239374
RNY9999 <rennya090611@gmail.com>
2026-02-06 19:08:20 +0900

    #17 feat: ログイン画面の実装 / 新規登録画面バリデーションチェック＆パスワード表示・非表示切り替えの実装

--- admin-ui/src/app/(public)/register/page.tsx
+++ admin-ui/src/app/(public)/register/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./register.module.css";
 import {
   Form,
@@ -13,36 +13,109 @@ import { useState } from "react";
 import Link from "next/link";
 import { routes } from "@/constants/routes";
 import { emailSchema, passwordSchema } from "@/schemas";
-import {z} from 'zod';
 
 const RegisterPage = () => {
   const [errorMessage, setErrorMessage] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [emailError, setEmailError] = useState<string>('');
-  const [passwordlError, setPasswordError] = useState<string>('');
-
+  const [emailError, setEmailError] = useState<string>("");
+  const [passwordError, setPasswordError] = useState<string>("");
+  const resetEmailError = () => {
+    setEmailError("");
+  };
+  const resetPasswordError = () => {
+    setPasswordError("");
+  };
+  const emailErrorTimerRef = useRef<number | null>(null);
+  const passwordErrorTimerRef = useRef<number | null>(null);
+  
+  const emailId = "email";
+  const passwordId = "password";
+  const maxEmailLength = 150; // From上で入力可能な数
+  const maxPasswordLength = 50; // From上で入力可能な数
   const isValid = true;
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {};
+  
+  /**
+   * 入力値のセット＆バリデーション
+   *
+   * ▼ 処理概要
+   * 1. useRefに保存されている .current が存在する場合 window.clearTimeout で setTimeOut をストップする（ユーザが入力中の間はエラーメッセージは出さない）
+   * 2. 入力処理 email は前後の空白を trim, password はそのまま setState する
+   * 3. value.length === 0 の場合は エラーメッセージを即削除してリターン※後続の処理にはいく必要がない
+   * 4. useStateの値を使うと一つ前の値が取れるので, e.target.value を使用して safeParse※例外エラーをはかない
+   * 5. response.success === true の場合は エラーメッセージを即削除して明示的にリターン※今後後続の処理が追加されてもいかない
+   * 6. response.success === false の場合は setTimeOut で1秒の猶予を持たせて setState でエラーメッセージを入力
+   * @param e
+   * @returns
+   */
   const handleOnchange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const id = e.target.id;
     const value = e.target.value;
-    const emailId = 'email';
-    const passwordId = 'password';
-    const maxEmailLength = 150; // From上で入力可能な数
-    const maxPasswordLength = 50; // From上で入力可能な数
 
     switch (id) {
-      case emailId:
+      case emailId: {
+        // 入力があった場合、まずエラー文を消す
+        resetEmailError();
+        // 1. useRefに保存されている .current が存在する場合 window.clearTimeout で setTimeOut をストップする（ユーザが入力中の間はエラーメッセージは出さない）
+        if (emailErrorTimerRef.current) {
+          window.clearTimeout(emailErrorTimerRef.current);
+        }
+
+        // 2. 入力処理 email は前後の空白を trim, password はそのまま setState する
         if (value.length <= maxEmailLength) {
           setEmail(value.trim());
         }
-        break;
-      case passwordId:
-        if (value.length <= maxPasswordLength) {
-          setPassword(value);
+
+        // 3. value.length === 0 の場合は エラーメッセージを即削除してリターン※後続の処理にはいく必要がない
+        if (value.length === 0) return resetEmailError();
+
+        // 4. useStateの値を使うと一つ前の値が取れるので, e.target.value を使用して safeParse※例外エラーをはかない
+        const result = emailSchema.safeParse(value);
+
+        // 5. response.success === true の場合は エラーメッセージを即削除して明示的にリターン※今後後続の処理が追加されてもいかない
+        if (result.success) return resetEmailError();
+
+        // 6. response.success === false の場合は setTimeOut で1秒の猶予を持たせて setState でエラーメッセージを入力
+        if (!result.success) {
+          emailErrorTimerRef.current = window.setTimeout(() => {
+            setEmailError(result.error?.issues[0].message);
+          }, 1000);
         }
         break;
+      }
+      case passwordId: {
+        // 入力があった場合、まずエラー文を消す
+        resetPasswordError();
+        // 1. useRefに保存されている .current が存在する場合 window.clearTimeout で setTimeOut をストップする（ユーザが入力中の間はエラーメッセージは出さない）
+        if (passwordErrorTimerRef.current) {
+          window.clearTimeout(passwordErrorTimerRef.current);
+        }
+
+        // 2. 入力処理 email は前後の空白を trim, password はそのまま setState する
+        if (value.length <= maxPasswordLength) {
+          console.log(typeof value);
+          setPassword(value);
+        }
+
+        // 3. value.length === 0 の場合は エラーメッセージを即削除してリターン※後続の処理にはいく必要がない
+        if (value.length === 0) return resetPasswordError();
+
+        // 4. useStateの値を使うと一つ前の値が取れるので, e.target.value を使用して safeParse※例外エラーをはかない
+        const result = passwordSchema.safeParse(value);
+
+        // 5. response.success === true の場合は エラーメッセージを即削除して明示的にリターン※今後後続の処理が追加されてもいかない
+        if (result.success) return resetPasswordError();
+
+        // 6. response.success === false の場合は setTimeOut で1秒の猶予を持たせて setState でエラーメッセージを入力
+        if (!result.success) {
+          passwordErrorTimerRef.current = window.setTimeout(() => {
+            setPasswordError(result.error?.issues[0].message);
+          }, 1000);
+        }
+        break;
+      }
       default:
         // email, password以外の場合は何もしない
         break;
@@ -51,25 +124,6 @@ const RegisterPage = () => {
   const closeErrorField = () => {
     setErrorMessage("");
   };
-
-  // email入力1秒後にバリデーションチェックを行う
-  useEffect(() => {
-    console.log('checkEmail');
-    setTimeout(() => {
-      const result = emailSchema.safeParse(email);
-      console.log(result.error?.issues);
-      console.log(result.error?.issues[0].message);
-      if (!result.success) {
-        setEmailError(result.error?.issues[0].message);
-      }
-    }, 1000);
-  }, [email]);
-  // password入力1秒後にバリデーションチェックを行う
-  useEffect(() => {
-    setTimeout(() => {
-      passwordSchema.safeParse(password)
-    }, 1000);
-  }, [password]);
   return (
     <div className={styles.register}>
       <h1 className={styles.register__title}>新規登録</h1>
@@ -80,9 +134,18 @@ const RegisterPage = () => {
       <Form onSubmit={handleSubmit}>
         <div className={styles.register__inputField}>
           <FormField label="ID（メールアドレス）" htmlFor="email">
-            <FormInput type="text" id="email" value={email} onChange={handleOnchange} />
+            <FormInput
+              type="text"
+              id="email"
+              value={email}
+              onChange={handleOnchange}
+            />
           </FormField>
-          {emailError}
+          <p
+            className={`${styles.register__text} ${styles["register__text--error"]}`}
+          >
+            {emailError}
+          </p>
         </div>
         <div className={styles.register__inputField}>
           <FormField label="パスワード" htmlFor="password">
@@ -91,8 +154,15 @@ const RegisterPage = () => {
               id="password"
               value={password}
               onChange={handleOnchange}
+              isPassword={true}
             />
+
           </FormField>
+          <p
+            className={`${styles.register__text} ${styles["register__text--error"]}`}
+          >
+            {passwordError}
+          </p>
           <p className={styles.register__text}>
             半角英数字と記号を組み合わせてください。
             <br />
--- admin-ui/src/schemas/password.schema.ts
+++ admin-ui/src/schemas/password.schema.ts
@@ -2,10 +2,8 @@ import z from 'zod';
 
 const PASSWORD_REGEX =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!-/:-@[-`{-~])[a-zA-Z0-9!-/:-@[-`{-~]+$/;
 
-export const passwordSchema = z.strictObject({
-  password: z
+export const passwordSchema = z
     .string()
     .min(6, 'パスワードは6文字以上で設定してください。')
     .max(20, 'パスワードは20文字以下で設定してください。')
-    .regex(PASSWORD_REGEX, 'パスワードは英大文字・小文字・数字・記号をそれぞれ1文字以上含めてください。')
-});
+    .regex(PASSWORD_REGEX, 'パスワードは英大文字・小文字・数字・記号をそれぞれ1文字以上含めてください。');
